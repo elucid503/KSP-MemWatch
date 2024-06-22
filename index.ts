49b54fafa177dc95979ce5839340f18e65b80841
= <paulujlaky@gmail.com>
2024-06-22 12:28:45 -0400

    Changes for better restart periods

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ let LastWebhookTime = 0;
 const RESTART_COOLDOWN = 300000; // 5 minutes
 const WEBHOOK_COOLDOWN = 5000; // 5 seconds
 
-async function Log(Title: string, Message: string) {
+async function Log(Title: string, Message: string, Color: number = 0xbda4fc) {
 
     console.log(`${Title.toUpperCase()} • ${Message}`);
 
@@ -35,7 +35,7 @@ async function Log(Title: string, Message: string) {
 
                 footer: Config.EmbedFooter,
 
-                color: 0xbda4fc
+                color: Color
 
             }
 
@@ -70,7 +70,7 @@ setInterval(async () => {
     const Players = Server.CurrentState.CurrentPlayers;
     const MemoryUsed = Server.CurrentState.BytesUsed;
 
-    if (PreviousAmtOfPlayers > 0 && Players.length === 0 && Date.now() - LastRestartTime > RESTART_COOLDOWN) {
+    if (PreviousAmtOfPlayers > 0 && Players.length === 0 && Date.now() - LastRestartTime > RESTART_COOLDOWN && MemoryUsed / (1024 ** 3) >= Config.RoutineRestartMemoryLimit) {
 
         PreviousAmtOfPlayers = 0;
 
@@ -79,7 +79,7 @@ setInterval(async () => {
             if (err) Log("Restart Server: Error", err.message);
             if (stderr) Log("Restart Server: Error", stderr);
 
-            Log("Server Restart", `Routinely restarted the server due to player count.`);
+            Log("Server Restart", `Routinely restarted the server due to player count and memory usage.`, 0xe874c1);
 
         });
 
@@ -96,7 +96,7 @@ setInterval(async () => {
             if (err) Log("Restart Server: Error", err.message);
             if (stderr) Log("Restart Server: Error", stderr);
 
-            Log("Server Restart", `Restarted the server due to high memory usage.`);
+            Log("Server Restart", `Restarted the server due to high memory usage.`, 0xe87489);
 
         });
 
@@ -104,7 +104,8 @@ setInterval(async () => {
 
     else if (MemoryUsed / (1024 ** 3) > Config.MemoryWarningThreshold) {
 
-        Log("Memory Warning", `The server is using ${(MemoryUsed / (1024 ** 3)).toFixed(3)} GB of memory.`);
+        Log("Memory Warning", `The server is using ${(MemoryUsed / (1024 ** 3)).toFixed(3)} GB of memory.`, 0xeaef7f);
+        
     }
 
     else if (LastRoutineLog + Config.RoutineLoggingTime < Date.now()) {
